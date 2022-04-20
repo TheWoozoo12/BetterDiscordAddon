69702bdb23d8ac1e280ace6bd39989f1c4d2aa34
TheWoozoo12 <69330974+TheWoozoo12@users.noreply.github.com>
2022-04-21 00:53:44 +0200

    Create example.plugin.js

---
+++ example.plugin.js
@@ -0,0 +1,14 @@
+/**
+ * @name Example
+ * @version 1.0.0
+ * @description Description of my Addon
+ * @author Plugin Author
+ * @website https://betterdiscord.app/
+ * @donate https://youtu.be/QqknSms8VVI?t=336
+*/
+
+module.exports = class ExamplePlugin {
+    load() {}
+    start() {}
+    stop() {}
+}
